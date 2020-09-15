963b03addf3d209d6594ff8555dadc587e8c1d95
iwao0313 <ik0313hw@gmail.com>
2020-09-15 12:46:56 +0900

    ユーザー編集機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root "item#index"
+  resources :items, only:[:new, :create, :show, :edit, :update, :destory] do
+    resources :purchases, only:[:index, :create]
+  end
 end
