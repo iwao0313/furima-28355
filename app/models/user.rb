9d709d5187d32bdfa57e49bd2fa0d84e436598b7
iwao0313 <ik0313hw@gmail.com>
2020-09-18 16:09:34 +0900

    テストコード準備

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         has_many :items
+         has_many :purchases
+
+        #  with_options presence: true do
+           
+  # validate :nickname
+  # validate :email
+  # validate :password, length: {minimum: 6}
+
+          #  end
 end
