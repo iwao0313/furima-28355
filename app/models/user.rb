f1b2c34aa87a26b0484b5100152c01764393ded5
iwao0313 <ik0313hw@gmail.com>
2020-09-19 18:26:12 +0900

    新規登録、ログイン実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,25 @@ class User < ApplicationRecord
 
          has_many :items
          has_many :purchases
+ 
+         with_options presence: true do
+  
+  validates :nickname
+  validates :email
+  validates :password, length: {minimum: 6}
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-        #  with_options presence: true do
-           
-  # validate :nickname
-  # validate :email
-  # validate :password, length: {minimum: 6}
+  validates :password_confirmation
+  validates :birth_date
 
-          #  end
+  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
+  validates :first_name
+  validates :last_name
+     end
+  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+  validates :first_name_kana
+  validates :last_name_kana
+     end
+   end
 end
