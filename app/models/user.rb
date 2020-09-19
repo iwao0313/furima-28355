bc146b44ef6c95b0577ace579e17efc992a430ab
iwao0313 <ik0313hw@gmail.com>
2020-09-19 20:40:04 +0900

    Rubocopの実施

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,27 +4,26 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :items
-         has_many :purchases
- 
-         with_options presence: true do
-  
-  validates :nickname
-  validates :email
-  validates :password, length: {minimum: 6}
-  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  has_many :items
+  has_many :purchases
 
-  validates :password_confirmation
-  validates :birth_date
+  with_options presence: true do
+    validates :nickname
+    validates :email
+    validates :password, length: { minimum: 6 }
+    VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i.freeze
+    validates :password, format: { with: VALID_PASSWORD_REGEX }
 
-  with_options format: {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
-  validates :first_name
-  validates :last_name
-     end
-  with_options format: {with: /\A[ァ-ヶー－]+\z/} do
-  validates :first_name_kana
-  validates :last_name_kana
-     end
-   end
+    validates :password_confirmation
+    validates :birth_date
+
+    with_options format: { with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
+  end
 end
