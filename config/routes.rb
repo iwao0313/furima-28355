ef5db25bde4f8553da3dca7ebab1576c41fa120c
iwao0313 <ik0313hw@gmail.com>
2020-09-22 17:24:44 +0900

    ルーティング記述修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root "items#index"
   devise_for :users
-  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
+  resources :items, except: [:index] do
     resources :purchases, only:[:index, :create]
   end
 end
