8811b3eed38ebe5ab6fb2c493d0f371a2f558946
iwao0313 <ik0313hw@gmail.com>
2020-09-22 18:40:52 +0900

    ビューファイルの作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,7 +5,7 @@ class ItemsController < ApplicationController
 
   def index
     @items = Item.all.order('created_at DESC')
-    # @purchase = PurchaseAddress.new
+    @purchase = PurchaseAddress.new
   end
 
 
