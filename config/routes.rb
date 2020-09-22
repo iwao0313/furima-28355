f45fd7f322da9f5f88f8281195b6506be7957502
iwao0313 <ik0313hw@gmail.com>
2020-09-22 16:38:24 +0900

    削除機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "items#index"
-  resources :items, only:[:new, :create, :show, :edit, :update, :destory] do
+  devise_for :users
+  resources :items, only: [:new, :create, :show, :edit, :update, :destroy] do
     resources :purchases, only:[:index, :create]
   end
 end
