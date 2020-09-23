ae0c23581380035c08e1da916262a9d5dee99b90
iwao0313 <ik0313hw@gmail.com>
2020-09-23 18:14:19 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_20_110341) do
+ActiveRecord::Schema.define(version: 2020_09_22_095014) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,11 @@ ActiveRecord::Schema.define(version: 2020_09_20_110341) do
     t.integer "ship_form_id"
     t.integer "shipping_days_id"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "purchase_addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
