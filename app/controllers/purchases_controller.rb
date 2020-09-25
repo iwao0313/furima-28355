a5de6a595ae343b860f482162b6883d7b91400cf
iwao0313 <ik0313hw@gmail.com>
2020-09-25 17:12:38 +0900

    Update purchases_controller.rb

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -51,7 +51,7 @@ class PurchasesController < ApplicationController
   end
 
   def pay_item
-    payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: params[:token],   
